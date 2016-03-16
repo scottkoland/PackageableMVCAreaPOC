c0c54faa3000ebb8e5eb64e5199157de73cd540e
Scott Koland <skoland@compositgroup.com>
2016-03-16 14:44:15 -0700

    Added Area controller.

---
+++ PackageableMVCAreaPOC/Areas/Area51/Controllers/AlienController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PackageableMVCAreaPOC.Areas.Area51.Controllers
+{
+    public class AlienController : Controller
+    {
+        // GET: Area51/Alien
+        public ActionResult Index()
+        {
+            return View(new [] {"gazoo", "kang", "kodos"});
+        }
+    }
+}
